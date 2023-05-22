22c47131aa415c125ef74b3f68ef43e3a6c8bd3c
thisisfoobar <matt.mcelreath26@gmail.com>
2023-05-22 15:21:24 -0400

    numbers lesson

---
+++ basics/numbers.js
@@ -0,0 +1,11 @@
+let age = 29
+let dogYears = (age + 1) / 7
+
+console.log(dogYears)
+
+// challenge
+let studentScore = 18
+let maxScore = 20
+let percent = (studentScore / maxScore) * 100
+
+console.log(percent)
