3885d558062662566f181029a8dc8aafbbd47924
thisisfoobar <matt.mcelreath26@gmail.com>
2023-05-22 15:10:18 -0400

    Strings lesson

---
+++ basics/strings.js
@@ -0,0 +1,12 @@
+let firstName = 'Matt'
+let lastName = 'Evergreen'
+let fullName = firstName + ' ' + lastName
+
+console.log(fullName)
+
+//Challenge
+let city = 'Waynesville'
+let country = 'United States'
+let location = city + ', ' + country
+
+console.log(location)
