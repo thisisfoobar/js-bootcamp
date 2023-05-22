1521b80d8f941f00bbd9eec9a61a5d8cede5aee6
thisisfoobar <matt.mcelreath26@gmail.com>
2023-05-22 15:27:46 -0400

    variable rules lesson

---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+// 1. You can't define a variable more than once
+let petName = 'Gene'
+petName = 'Spot'
+
+console.log(petName)
+
+// 2. There are rules related to variable names
+// 2a. Variable name has to start with a letter or $ or _
+let a1$_
+
+// 3. Variable names cannot be reserved keywords
